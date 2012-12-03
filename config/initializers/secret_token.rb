7f3959dc28c3aaa548b2a716c1f63d1122430403
Chris Goodmacher <cgoodmac@gmail.com>
2012-12-03 11:52:09 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Staqd::Application.config.secret_token = '0814f68bf15106aec10d43b9df41ed4a5229f1447ca44c9d19db9ba04dc4873681a5f9f63046b4e777439065ff34eb3a39ab01bd7e57eb3535ee5343734bef5b'
